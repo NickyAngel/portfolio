200ccfff32d4dbccf751d0bdccb31c6cb83f70e2
Nick Angelopoulos <neangelopoulos@gmail.com>
2023-02-02 18:52:46 -0700

    implemented basic persistent navbar

---
+++ components/Navbar.js
@@ -0,0 +1,47 @@
+import Link from "next/link";
+import Image from "next/image";
+import React, { useState } from "react";
+import NavItem from "./NavItem";
+
+const MENU_LIST = [
+  { text: "Home", href: "/" },
+  { text: "About Us", href: "/about" },
+  { text: "VibeCheck Demo", href: "/vibecheck" },
+];
+const Navbar = () => {
+  const [navActive, setNavActive] = useState(false);
+  const [activeIdx, setActiveIdx] = useState(-1);
+
+  return (
+    <header>
+      <nav className={`nav`}>
+        <Link href={"/"}>
+          <h1 className="logo">Nicholas Angelopoulos</h1>
+        </Link>
+        <div
+          onClick={() => setNavActive(!navActive)}
+          className={`nav__menu-bar`}
+        >
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+        <div className={`${navActive ? "active" : ""} nav__menu-list`}>
+          {MENU_LIST.map((menu, idx) => (
+            <div
+              onClick={() => {
+                setActiveIdx(idx);
+                setNavActive(false);
+              }}
+              key={menu.text}
+            >
+              <NavItem active={activeIdx === idx} {...menu} />
+            </div>
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
