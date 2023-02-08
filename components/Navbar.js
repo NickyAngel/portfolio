cc310d386e5726cb36b06c09884251b26da891ff
Nick Angelopoulos <neangelopoulos@gmail.com>
2023-02-08 14:58:45 -0700

    added resume link to navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -7,7 +7,7 @@ const MENU_LIST = [
   { text: "Home", href: "/#home" },
   { text: "About Me", href: "/#about" },
   { text: "VibeCheck Demo", href: "/#vibecheck" },
-  { text: "Contact Me", href: "/#contact"}
+  { text: "Contact Me", href: "/#contact" },
 ];
 const Navbar = ({ themeVal, setTheme }) => {
   return (
@@ -31,6 +31,13 @@ const Navbar = ({ themeVal, setTheme }) => {
                 <NavItem {...menu} />
               </div>
             ))}
+            <a
+              href="/resume.pdf"
+              target="_blank"
+              className="ml-1 mt-[1.8rem] mb-1.5 rounded-full border-2 border-gray-800 bg-sky-300 px-2 text-sky-800 hover:cursor-pointer hover:bg-sky-600 dark:border-gray-600 dark:bg-sky-50/0 dark:text-sky-700 dark:hover:bg-gray-400"
+            >
+              Resume
+            </a>
           </div>
         </div>
         <div className="flex min-h-full items-center px-0.5 hover:cursor-pointer">
