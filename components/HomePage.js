b450c6369ca56c31156c8c7a8af7aaadbd95f51d
Nick Angelopoulos <neangelopoulos@gmail.com>
2023-02-08 14:02:23 -0700

    home styling starting to get there

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,11 +1,20 @@
 export default function HomePage() {
   return (
-    <div className="flex justify pt-40">
+    <div className="justify ml-10 flex pt-40">
       <div className="">
-        <p>Hello, my name is</p>
-        <h1 className="text-5xl">Nicholas Angelopoulos</h1>
+        <p className="mb-2">Hey there, you can call me:</p>
+        <h1 className="text-5xl dark:text-sky-300">Nicholas Angelopoulos,</h1>
+        <h1 className="text-4xl text-gray-800 dark:text-sky-300/50">Nicky Angel,</h1>
+        <h1 className="text-3xl text-gray-600 dark:text-sky-600/50">your best friend,</h1>
+        <h1 className="text-3xl text-gray-400 dark:text-sky-900/50">
+          or really anything you'd like.
+        </h1>
         <br />
-        <p>Welcome to my portfolio site. Feel free to scroll or click around to explore!</p>
+        <p className="w-4/5">
+          I'm a software engineer who uses JavaScript to do full-stack web
+          development. Currently I am looking for a new position, and hoping to
+          join a team of enthusiastic people who enjoy coding as much as I do.
+        </p>
       </div>
     </div>
   );
