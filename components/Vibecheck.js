335470924942deff9e7cc084c8dd731774acd468
Nick Angelopoulos <neangelopoulos@gmail.com>
2023-02-09 14:27:41 -0700

    more responsive design on vibecheck component

--- components/Vibecheck.js
+++ components/Vibecheck.js
@@ -3,25 +3,25 @@ import { FiGithub } from "react-icons/fi";
 export default function Vibe() {
   return (
     <div className="">
-      <div className=" mx-5  md:ml-10 flex flex-col pt-3">
+      <div className=" mx-5  flex flex-col pt-3 md:ml-10">
         <h1 className="text-4xl">vibe check</h1>
         <h2 className="text-2xl">a micro journaling app</h2>
       </div>
-      <div className="flex mx-5 md:ml-10 pt-5">
+      <div className="mx-5 flex pt-5 md:mx-10">
         <div className="min-w-full">
           <div>
             <h3 className="text-xl">check out the video demo here!</h3>
           </div>
           <div className="flex justify-center">
             <iframe
-              className="aspect-video w-full mr-10 rounded-3xl"
+              className="aspect-video w-full  rounded-3xl"
               src="https://www.youtube.com/embed/ZkAHRSJC-rw"
               title="YouTube video player"
               allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
             ></iframe>
           </div>
-          <div className="flex flex-row justify-around py-2">
+          <div className="flex flex-row justify-around py-2 text-xs md:text-lg">
             <a
               className="rounded-full bg-sky-200 p-3 shadow-lg hover:bg-sky-400 dark:shadow-sky-500/30"
               href="https://vibe-check.onrender.com/"
