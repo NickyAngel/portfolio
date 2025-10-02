e3bc55e83fe734da193cea9e6381e9196fcfbb8b
Nicholas Angelopoulos <105730827+NickyAngel@users.noreply.github.com>
2025-10-02 14:36:17 -0600

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -35,13 +35,6 @@ const Navbar = ({ themeVal, setTheme }) => {
                 <NavItem {...menu} />
               </a>
             ))}
-            <a
-              href="/resume.pdf"
-              target="_blank"
-              className="mt-8 ml-1 mb-1.5 rounded-full border-gray-800 bg-sky-300 px-2 text-sky-800 hover:cursor-pointer hover:bg-sky-600 dark:border-gray-600 dark:bg-sky-900 dark:text-sky-600 dark:hover:bg-gray-400 lg:mt-[1.8rem] lg:border-2"
-            >
-              Resume
-            </a>
           </div>
         </div>
         <div className="flex min-h-full items-center px-0.5 hover:cursor-pointer">
